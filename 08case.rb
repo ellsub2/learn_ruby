cb6f94b44c8c5e93e13593896cf0b4431754275e
orion3939 <k.izumi3939@gmail.com>
2016-01-02 01:33:28 +0900

    p78

---
+++ 08case.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/ruby
+#encoding: UTF-8
+
+tags = [ "A", "IMG", "PRE" ]
+tags.each do |tagname|
+	case tagname
+	when "P", "A", "D", "HOGE"
+		puts "its a small world."
+	when "IMG", "THE"
+		puts "its a big world."
+	else
+		puts "HOGE HOGE"
+	end
+end
+
+array = [ "A", 1, nil]
+array.each do |item|
+	case item
+	when String
+		puts "#{item} is String."
+	when Numeric
+		puts "#{item} is Numeric."
+	else
+		puts "#{item} is not something."
+	end
+end
+
