e06483c9c2bcfffca3df83a6c26d4eb0120a01df
heliumu <heliumu2525@gmail.com>
2016-01-05 15:17:09 +0900

    change:comment

--- 10method_hello.rb
+++ 10method_hello.rb
@@ -2,9 +2,14 @@
 #encoding: UTF-8
 
 #define method----------------
-def hello(name)
+#引数のデフォルト値は、右から与えてゆくこと。
+#左端や途中の引数を省略することは不可能。
+#例;	def hello(name1, name2="Ruby") 	#これはok
+#		def hello(name1="Ruby", name2)	#これはNG
+def hello(name="Ruby")
 	puts "hello, #{name}!"
 end
 
 #use method-------------------
-hello(ARGV[0])
+hello()			#引数を省略して呼び出し
+hello("scala")	#引数を与えて呼び出し
