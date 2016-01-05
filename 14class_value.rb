6936e44155ec417d819e45ab4f4b1a8d3add19fe
heliumu <heliumu2525@gmail.com>
2016-01-05 20:13:00 +0900

    new

---
+++ 14class_value.rb
@@ -0,0 +1,40 @@
+#たのしいRuby 第4版 p140
+#encoding: UTF-8
+
+#変数selfとクラスメソッドがわからない。
+#たぶんこれらは密接な関係があるように感じまふ。
+
+#クラスメソッドはクラスに関しての操作?
+
+class HelloCount
+	@@count = 0	#helloメソッドの呼び出し回数
+	
+	#@@count参照用のクラスメソッド。クラス変数(@@云々の形)はattr.で定義できない。
+	#たぶんself.countと書いても問題ないはず。
+	def HelloCount.count 
+		@@count
+	end
+
+	def initialize(myname="Ruby")
+		@name = myname
+	end 
+
+	def hello 
+		@@count += 1							#呼び出し回数を加算
+		puts "Hello, #{@name}.\n"
+	end 
+end 
+
+#object生成-------------------------
+bob = HelloCount.new("Bob")
+alice = HelloCount.new("alice")
+ruby = HelloCount.new
+
+p HelloCount.count #=> 0 
+
+#helloメソッドを使ってみる----------
+bob.hello 
+alice.hello 
+ruby.hello 
+
+p HelloCount.count #=> 3
